ab85833da03418d014dc82f324db842a548943d3
Seremba Patrick <45463621+Seremba@users.noreply.github.com>
2023-05-29 10:37:34 +0000

    initial commit

---
+++ betterreads-data-loader/src/main/java/com/seremba/betterreadsdataloader/BetterreadsDataLoaderApplication.java
@@ -0,0 +1,14 @@
+package com.seremba.betterreadsdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BetterreadsDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterreadsDataLoaderApplication.class, args);
+	}
+
+}
