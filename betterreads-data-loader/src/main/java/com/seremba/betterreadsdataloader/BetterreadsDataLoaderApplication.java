4e9d3db302b54cc4b26f824921fa15e5662e14c7
Seremba Patrick <pserembae@gmail.com>
2023-05-31 09:05:15 +0300

    map book_id to author

--- betterreads-data-loader/src/main/java/com/seremba/betterreadsdataloader/BetterreadsDataLoaderApplication.java
+++ betterreads-data-loader/src/main/java/com/seremba/betterreadsdataloader/BetterreadsDataLoaderApplication.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.annotation.PostConstruct;
@@ -13,6 +17,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.configurationprocessor.json.JSONArray;
 import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -20,6 +25,7 @@ import org.springframework.context.annotation.Bean;
 
 import com.seremba.betterreadsdataloader.author.Author;
 import com.seremba.betterreadsdataloader.author.AuthorRepository;
+import com.seremba.betterreadsdataloader.book.Book;
 import com.seremba.betterreadsdataloader.connection.DataStaxAstraProperties;
 
 @SpringBootApplication
@@ -73,13 +79,55 @@ public class BetterreadsDataLoaderApplication {
 		Path path = Paths.get(worksDatadumpLocation);
 		
 		try (Stream<String> lines = Files.lines(path)) {
-			lines.limit(10).forEach(line -> {
+			lines.limit(50).forEach(line -> {
 				String jsonString = line.substring(line.indexOf("{"));
 				try {
-					new JSONObject(jsonString);
+					JSONObject jsonObject = new JSONObject(jsonString);
+					
+					Book book = new Book();
+					book.setId(jsonObject.getString("key").replace("/works/", ""));
+					book.setName(jsonObject.optString("title"));
+					JSONObject descripionObject = jsonObject.optJSONObject("description");
+					if (descripionObject != null) {
+						book.setDescription(descripionObject.optString("value"));
+					}
+					
+					JSONArray authorsJSONArray = jsonObject.optJSONArray("authors");
+					if (authorsJSONArray != null) {
+						List<String> authorIds = new ArrayList<>();
+						for (int i = 0; i < authorsJSONArray.length(); i++) {
+							String authorId = authorsJSONArray.getJSONObject(i).getJSONObject("author").getString("key")
+							        .replace("/authors/",
+							    "");
+							authorIds.add(authorId);
+						}
+						book.setAuthorIds(authorIds);
+						List<String> authorNames = authorIds.stream().map(id -> authorRepository.findById(id))
+						        .map(optionalAuthor -> {
+							        if (!optionalAuthor.isPresent())
+								        return "Unknown Author";
+							        return optionalAuthor.get().getName();
+						        }).collect(Collectors.toList());
+						book.setAuthorNames(authorNames);
+					}
+					
+					
+					JSONArray coversJSONArray = jsonObject.optJSONArray("covers");
+					if (coversJSONArray != null) {
+						List<String> coverIds = new ArrayList<>();
+						for (int i = 0; i < coversJSONArray.length(); i++) {
+							coverIds.add(coversJSONArray.getString(i));
+						}
+						book.setCoverIds(coverIds);
+					}
+					JSONObject publishedObject = jsonObject.optJSONObject("created");
+					if (publishedObject != null) {
+						String dateStr = publishedObject.optString("value");
+						book.setPubishedDate(LocalDate.parse(dateStr));
+					}
 					
 				}
-				catch (JSONException e) {
+				catch (Exception e) {
 					e.printStackTrace();
 				}
 			});
@@ -93,7 +141,8 @@ public class BetterreadsDataLoaderApplication {
 	
 	@PostConstruct
 	public void start() {
-		initAuthor();
+		//		initAuthor();
+		initWorks();
 	}
 	
 	@Bean
